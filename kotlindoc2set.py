ccb40d9de60e739b784ae47934f5853c86a91f53
Tyler B. Thrailkill <tylerbthrailkill@gmail.com>
2024-03-08 22:43:59 -0700

    Add Table Of Contents capability to docset
    adds table of contents to every page, with a custom mapping to Dash's
    internal entry types. I left the map in place so that we can adjust the
    type if needed later, if something else makes more sense.
    
    adds custom css so that it does not need to be maintained as much.

--- kotlindoc2set.py
+++ kotlindoc2set.py
@@ -5,16 +5,17 @@ from sqliteconnection import SQLiteConnection
 
 if __name__ == '__main__':
 
-    dirutil.setup_docset_directories(DOCSET_DOCUMENT_PATH)
-    dirutil.copy_icon(STATIC_ASSET_ICON_PATH, DOCSET_ICON_PATH)
-    dirutil.copy_plist(STATIC_ASSET_PLIST_PATH, DOCSET_PLIST_PATH)
+    # dirutil.setup_docset_directories(DOCSET_DOCUMENT_PATH)
+    # dirutil.copy_icon(STATIC_ASSET_ICON_PATH, DOCSET_ICON_PATH)
+    # dirutil.copy_plist(STATIC_ASSET_PLIST_PATH, DOCSET_PLIST_PATH)
 
     sqlite_connection: SQLiteConnection = SQLiteConnection(DATABASE_PATH)
-    sqlite_connection.drop_sqlite_search_index()
-    sqlite_connection.create_sqlite_search_index()
+    # sqlite_connection.drop_sqlite_search_index()
+    # sqlite_connection.create_sqlite_search_index()
 
     web_parser: KotlinWebDocParser = KotlinWebDocParser(WEB_DOCS_URL, DOCSET_DOCUMENT_PATH, sqlite_connection)
-    web_parser.mirror_website()
+    # web_parser.mirror_website()
     web_parser.parse()
+    web_parser.add_custom_css()
 
     sqlite_connection.commit_and_close()
--- kotlinwebdocparser.py
+++ kotlinwebdocparser.py
@@ -1,9 +1,146 @@
 import os
 import re
+import glob
 from subprocess import call
 from bs4 import BeautifulSoup
 
 from sqliteconnection import SQLiteConnection
+
+mapping_dict = {
+    "Annotations": "Annotation",
+    # "Companion Object Extension Properties": "Attribute",
+    # "TODO": "Binding",
+    # "TODO": "Builtin",
+    # "TODO": "Callback",
+    # "TODO": "Category",
+    # "TODO": "Class",
+    # "TODO": "Command",
+    # "TODO": "Component",
+    "Companion Object Properties": "Constant",
+    "Constructors": "Constructor",
+    # "TODO": "Define",
+    # "TODO": "Delegate",
+    # "TODO": "Diagram",
+    # "TODO": "Directive",
+    # "TODO": "Element",
+    # "TODO": "Entry",
+    "Enum Values": "Enum",
+    # "TODO": "Environment",
+    # "TODO": "Error",
+    # "TODO": "Event",
+    "Exceptions": "Exception",
+    "Extensions for External Classes": "Extension",
+    "Extension Functions": "Extension",
+    "Companion Object Extension Properties": "Field",
+    "Extension Properties": "Field",
+    "Extensions for java.nio.file.Path": "Field",
+    "Extensions for java.net.URI": "Field",
+    "Extensions for java.math.BigDecimal": "Field",
+    "Extensions for java.math.BigInteger": "Field",
+    "Extensions for java.util.stream.DoubleStream": "Field",
+    "Extensions for kotlin.sequences.Sequence": "Field",
+    "Extensions for java.util.stream.IntStream": "Field",
+    "Extensions for java.util.stream.LongStream": "Field",
+    "Extensions for java.util.stream.Stream": "Field",
+    "Extensions for java.util.Optional": "Field",
+    "Extensions for java.util.concurrent.ConcurrentMap": "Field",
+    "Extensions for java.util.Enumeration": "Field",
+    "Extensions for java.util.regex.Pattern": "Field",
+    "Extensions for java.io.OutputStream": "Field",
+    "Extensions for java.io.InputStream": "Field",
+    "Extensions for java.lang.reflect.Method": "Field",
+    "Extensions for java.lang.reflect.Constructor": "Field",
+    "Extensions for java.lang.reflect.Field": "Field",
+    "Extensions for java.lang.concurrent.ConcurrentMap": "Field",
+    "Extensions for java.lang.StringBuilder": "Field",
+    "Extensions for java.lang.Appendable": "Field",
+    "Extensions for java.lang.Class": "Field",
+    "Extensions for java.lang.Field": "Field",
+    "Extensions for java.lang.Method": "Field",
+    "Extensions for java.lang.ThreadLocal": "Field",
+    "Extensions for java.lang.Enum.getDeclaringClass": "Field",
+    "Extensions for java.lang.reflect.AnnotatedElement.getAnnotationsByType": "Field",
+    "Extensions for java.lang.annotation.Repeatable": "Field",
+    "Extensions for java.lang.Class.getName": "Field",
+    "Extensions for java.reflect.Constructor": "Field",
+    "Extensions for java.reflect.Field": "Field",
+    "Extensions for java.reflect.Method": "Field",
+    "Extensions for java.lang.reflect.AccessibleObject": "Field",
+    "Extensions for java.util.Random": "Field",
+    "Extensions for java.util.concurrent.locks.Lock": "Field",
+    "Extensions for java.util.Timer": "Field",
+    "Extensions for java.util.concurrent.locks.ReentrantReadWriteLock": "Field",
+    "Extensions for java.time.Duration": "Field",
+    # "TODO": "File",
+    # "TODO": "Filter",
+    # "TODO": "Framework",
+    "Functions": "Function",
+    "Inherited Functions": "Function",
+    # "TODO": "Global",
+    # "TODO": "Guide",
+    # "TODO": "Hook",
+    # "TODO": "Instance",
+    # "TODO": "Instruction",
+    # "TODO": "Interface",
+    # "TODO": "Keyword",
+    # "TODO": "Library",
+    # "TODO": "Literal",
+    # "TODO": "Macro",
+    "Companion Object Functions": "Method",
+    "Inherited Properties": "Mixin",
+    # "TODO": "Modifier",
+    "Companion Object Extension Functions": "Module",
+    # "TODO": "Namespace",
+    # "TODO": "Notation",
+    # "TODO": "Object",
+    # "TODO": "Operator",
+    # "TODO": "Option",
+    "Packages": "Package",
+    # "TODO": "Parameter",
+    # "TODO": "Plugin",
+    # "TODO": "Procedure",
+    "Properties": "Property",
+    # "TODO": "Protocol",
+    "Inheritors": "Provider",
+    # "TODO": "Provisioner",
+    # "TODO": "Query",
+    # "TODO": "Record",
+    # "TODO": "Resource",
+    # "TODO": "Sample",
+    # "TODO": "Section",
+    # "TODO": "Service",
+    # "TODO": "Setting",
+    # "TODO": "Shortcut",
+    # "TODO": "Statement",
+    # "TODO": "Struct",
+    # "TODO": "Style",
+    # "TODO": "Subroutine",
+    # "TODO": "Tag",
+    # "TODO": "Test",
+    # "TODO": "Trait",
+    "Types": "Type",
+    # "TODO": "Union",
+    # "TODO": "Value",
+    # "TODO": "Variable",
+    # "TODO": "Word"
+}
+
+
+def get_custom_css() -> str:
+    return """
+    .docs-nav-controls > a {
+      display: none;
+    }
+    .global-layout>div>header{
+      display: none;
+    }
+    .scroll-button-back{
+      display: none;
+    }
+    footer {
+      display: none;
+    }
+    """
 
 
 class KotlinWebDocParser:
@@ -30,22 +167,66 @@ class KotlinWebDocParser:
     def parse(self):
         for dirpath, _, files in os.walk(self.local_path):
             for page in files:
+                # print("page => " + page)
                 if page.endswith('.html'):
                     self.parse_file(os.path.join(dirpath, page))
 
     def parse_file(self, file_path: str):
+        print("parsing file " + file_path)
         with open(file_path) as page:
             soup = BeautifulSoup(page.read(), features='html.parser')
-            for node in soup.find_all('div', attrs={'class': ['node-page-main', 'overload-group']}):
-                signature = node.find('div', attrs={'class': 'signature'})
-                if signature:
-                    code_type = self.parse_code_type(signature.text.strip())
-                    name_dom = soup.find('div', attrs={'class': 'api-docs-breadcrumbs'})
-                    name = '.'.join(map(lambda string: string.strip(), name_dom.text.split('/')[2::]))
-                    path = file_path.replace('kotlin.docset/Contents/Resources/Documents/', '')
-                    if code_type is not None and name:
-                        self.database.insert_into_index(name, code_type, path)
-                        print('%s -> %s -> %s' % (name, code_type, path))
+
+            self.add_toc(soup)
+
+            # for section in soup.find_all('h3', attrs={'id': 'properties'})
+            # for node in soup.find_all('div', attrs={'class': ['node-page-main', 'overload-group']}):
+            #     signature = node.find('div', attrs={'class': 'signature'})
+            #     if signature:
+            #         # print("signature is : " + signature.text.strip())
+            #         code_type = self.parse_code_type(signature.text.strip())
+            #         # print("code type is : " + code_type)
+            #         name_dom = soup.find('div', attrs={'class': 'api-docs-breadcrumbs'})
+            #         # print("name dom is : " + name_dom)
+            #         name = '.'.join(map(lambda string: string.strip(), name_dom.text.split('/')[2::]))
+            #         # print("name is : " + name)
+            #         path = file_path.replace('kotlin.docset/Contents/Resources/Documents/', '')
+            #         if code_type is not None and name:
+            #             self.database.insert_into_index(name, code_type, path)
+            #             print('%s -> %s -> %s' % (name, code_type, path))
+
+        # Now, overwrite the original file with the modified soup
+        with open(file_path, 'w') as file:
+            file.write(str(soup.prettify()))
+
+    def add_toc(self, soup):
+        sections = soup.find_all('h3')
+        for section in sections:
+            docset_links = section.find_all("a")
+            for docset_link in docset_links:
+                docset_link.decompose()
+            section_name = section.text.strip()
+            print("Found section with text {}".format(section_name))
+            new_tag = soup.new_tag("a",
+                                   attrs={'class': 'dashAnchor', 'name': f"//apple_ref/cpp/Section/{section_name}"})
+            section.append(new_tag)
+
+            container_with_things = section.find_next_sibling()
+            container_with_things_headers = container_with_things.find_all('h4')
+            for element_header in container_with_things_headers:
+                header_text = element_header.find('a').text.strip()
+                print(header_text)
+                if section_name.startswith("Extensions for java"):
+                    mapped_section = "Field"
+                else:
+                    mapped_section = mapping_dict[section_name]
+                print("mapping {} to {}".format(header_text, mapped_section))
+                old_elements = element_header.find_all("a", attrs={'class': 'dashAnchor'})
+                for old_element in old_elements:
+                    old_element.decompose()
+                new_tag = soup.new_tag("a", attrs={'class': 'dashAnchor',
+                                                   'name': f"//apple_ref/cpp/{mapped_section}/{header_text}"})
+
+                element_header.append(new_tag)
 
     def parse_code_type(self, code: str) -> str:
         tokens = list(filter(
@@ -77,3 +258,23 @@ class KotlinWebDocParser:
             return 'Constructor'
         elif re.match(r"[A-Z0-9\_]+", code):
             return 'Enum'
+
+    def add_custom_css(self):
+        """
+        Add custom css to the custom.css file. The only issue is that the common.css file is that the name can be
+        randomized at the end. So it can be named something like 'common.css?&v=be6472fafff6ae90f359922e40596cbb.css'
+
+        This function searches for that file, then adds the custom css declared at the top of this file.
+        :return:
+        """
+        css = get_custom_css()
+
+        # add to the common.css file, but it can have a random string in the middle like common.css?v=xxxxxx.css
+        files = glob.glob(os.path.join(self.local_path, '_assets/common.css*'))
+
+        for file_path in files:
+            with open(file_path, 'r') as file:
+                content = file.read()
+            if css not in content:
+                with open(file_path, 'a') as file:
+                    file.write(css)
