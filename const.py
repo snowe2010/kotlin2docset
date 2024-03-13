d4c874d30ab5a27edd6f92bed66aee306db19440
Tyler B. Thrailkill <tylerbthrailkill@gmail.com>
2024-03-13 17:09:36 -0600

    Add test library api documentation

--- const.py
+++ const.py
@@ -1,7 +1,9 @@
 WEB_DOCS_URL = 'https://kotlinlang.org/api/latest/jvm/stdlib/index.html'
+TEST_URL = 'https://kotlinlang.org/api/latest/kotlin.test/index.html'
 DATABASE_PATH = "kotlin.docset/Contents/Resources/docSet.dsidx"
 STATIC_ASSET_ICON_PATH = "./static/icon.ico"
 STATIC_ASSET_PLIST_PATH = "./static/Info.plist"
-DOCSET_DOCUMENT_PATH = "kotlin.docset/Contents/Resources/Documents/"
+DOCSET_DOCUMENT_PATH = "kotlin.docset/Contents/Resources/Documents/stdlib"
+DOCSET_TEST_DOCUMENT_PATH = "kotlin.docset/Contents/Resources/Documents/test"
 DOCSET_PLIST_PATH = "kotlin.docset/Contents/"
 DOCSET_ICON_PATH = "kotlin.docset/"
