2354a6f6876a22141a360b81f10e7819a045677f
Tyler B. Thrailkill <tylerbthrailkill@gmail.com>
2024-03-14 10:47:18 -0600

    Final update to enable full new docset

--- kotlindoc2set.py
+++ kotlindoc2set.py
@@ -5,7 +5,8 @@ from sqliteconnection import SQLiteConnection
 
 if __name__ == '__main__':
 
-    # dirutil.setup_docset_directories(DOCSET_DOCUMENT_PATH)
+    dirutil.setup_docset_directories(DOCSET_DOCUMENT_PATH)
+    dirutil.setup_docset_directories(DOCSET_TEST_DOCUMENT_PATH)
     dirutil.copy_icon(STATIC_ASSET_ICON_PATH, DOCSET_ICON_PATH)
     dirutil.copy_plist(STATIC_ASSET_PLIST_PATH, DOCSET_PLIST_PATH)
 
@@ -14,9 +15,9 @@ if __name__ == '__main__':
     sqlite_connection.create_sqlite_search_index()
 
     web_parser: KotlinWebDocParser = KotlinWebDocParser(WEB_DOCS_URL, DOCSET_DOCUMENT_PATH, sqlite_connection)
-    # web_parser.mirror_website()
-    # web_parser.parse()
-    # web_parser.add_custom_css()
+    web_parser.mirror_website()
+    web_parser.parse()
+    web_parser.add_custom_css()
 
     web_parser2: KotlinWebDocParser = KotlinWebDocParser(TEST_URL, DOCSET_TEST_DOCUMENT_PATH, sqlite_connection)
     web_parser2.mirror_website()
